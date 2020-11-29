2be74281bd3e82c9e6633299a11ccb6cb8e3ed67
unknown <marisela.delgadillo.cbtis37@gmail.com>
2020-11-28 21:16:06 -0700

    antes de la bd

---
+++ views/index.jsx
@@ -0,0 +1,44 @@
+import React, {Component, Fragment} from 'react';
+
+
+export default class index extends Component{
+    render() {
+      return (
+       <Fragment>
+        <div>
+          <base href="/" />
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+          <title>File Upload and Download</title>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossOrigin="anonymous" />
+          <link rel="stylesheet" href="/stylesheets/style.css" />
+          <my-app>Loading ...</my-app>
+          <script src="/js/app/bundle.js"></script>
+        </div>
+        </Fragment>
+    
+      );
+    }
+  };
+
+//   export default NewComponent();*/
+/*
+var NewComponent = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <base href="/" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <title>File Upload and Download</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossOrigin="anonymous" />
+        <link rel="stylesheet" href="/stylesheets/style.css" />
+        <my-app>Loading ...</my-app>
+      </div>
+    );
+  }
+});
+
+export default NewComponent();*/
